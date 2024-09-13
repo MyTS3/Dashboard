bc7b7b62fbb936d5a9969982e07f07b8f0767d7a
mehdi <mehdibigi1383@gmail.com>
2024-09-14 02:00:25 +0330

    loading component added /make server updated

--- components/modules/makeServer.vue
+++ components/modules/makeServer.vue
@@ -36,6 +36,7 @@
       >
       <from class="w-full my-4">
         <select
+          :disabled="disableInputs"
           class="w-full bg-transparent text-right appearance-none border rounded-xl p-3"
           v-model="selectedConfig"
           name="locations"
@@ -107,46 +108,7 @@
         >
           <span><img src="/images/plus.png" alt="" /></span>ساخت
         </div>
-        <svg
-          v-if="disableInputs"
-          class="h-6  "
-          version="1.1"
-          id="L3"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 100 100"
-          enable-background="new 0 0 0 0"
-          xml:space="preserve"
-        >
-          <circle
-            fill="none"
-            stroke="#fff"
-            stroke-width="10"
-            cx="50"
-            cy="50"
-            r="44"
-            style="opacity:0.5;"
-          />
-          <circle
-            fill="#fff"
-            stroke="#e74c3c"
-            stroke-width="10"
-            cx="8"
-            cy="54"
-            r="6"
-          >
-            <animateTransform
-              attributeName="transform"
-              dur="2s"
-              type="rotate"
-              from="0 50 48"
-              to="360 50 52"
-              repeatCount="indefinite"
-            />
-          </circle>
-        </svg>
+        <loading v-if="disableInputs" />
       </button>
     </main>
     <serverToken
@@ -162,6 +124,7 @@
 <script setup>
 import serverToken from "./serverToken.vue";
 import nuxtStorage from "nuxt-storage";
+import loading from "../loading .vue";
 ///////////////////////////////////////
 const serverTokenTab = ref(false);
 ////
