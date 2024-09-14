d6f18757c7f0dafba629d1bbc7e0d2258b304276
Daniel <daniel@sohe.ir>
2024-09-14 04:48:16 +0330

    refactor: format with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-    // Your custom configs here
-)
+export default withNuxt();
+// Your custom configs here
