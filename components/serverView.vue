6f09c88cd822f94db8c0b2380f9883bdbe01d558
Mehdi Beigi <mehdibigi1383@gmail.com>
2024-09-17 20:39:03 +0430

    setting up for yatqa example

--- components/serverView.vue
+++ components/serverView.vue
@@ -91,6 +91,7 @@
           <label class="button" for="server-status" />
         </div>
       </li>
+      <p class='text-right p-2 cursor-pointer text-main_orange' >مشاهده اطلاعات در صفحه یاتکا</p>
     </main>
     <footer class="grid grid-cols-2 gap-5 w-full absolute bottom-10 p-4">
       <button
