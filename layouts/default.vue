92ff1e6430ca38abe8d3390ca4de2beaf8a011c9
mehdi <mehdibigi1383@gmail.com>
2024-10-09 21:27:42 +0330

    now if not logged in redirect to login page

--- layouts/default.vue
+++ layouts/default.vue
@@ -162,8 +162,15 @@ import logoutPopup from '@/components/modules/logoutPopup.vue';
 const logoutTab = ref(false);
 const panelAlert = ref(true);
 const token = localStorage.getItem('token');
-const bodyToken = JSON.parse(atob(token.split('.')[1]));
-const name = ref(bodyToken.name);
+
+const name = ref();
+if (!token)
+  navigateTo('https://my.mtserver.ir/index.php?rp=/login', { external: true });
+else {
+  console.log('mio');
+  const bodyToken = JSON.parse(atob(token.split('.')[1]));
+  name.value = bodyToken.name;
+}
 
 TimeAgo.addLocale(fa);
 </script>
