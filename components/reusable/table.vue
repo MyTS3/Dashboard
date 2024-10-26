e3a611353c43ec92f1d6f27e86b5a2c2dee4bf72
Daniel <daniel@sohe.ir>
2024-10-26 17:34:30 +0330

    Revert "table change"
    This reverts commit 169505929872029e13b6a88df0a6539409c1d878.

--- components/reusable/table.vue
+++ components/reusable/table.vue
@@ -1,8 +1,6 @@
 <template>
-  <section dir="rtl" class="w-full grid h-full mx-auto text-white">
-    <main
-      class="w-full overflow-y-scroll flex flex-col bg-mainbg_500 rounded-2xl rounded-t-none"
-    >
+  <section dir="rtl" class="grid w-full h-full mx-auto text-white">
+    <main class="w-full flex flex-col bg-mainbg_500 rounded-2xl">
       <slot />
     </main>
   </section>
