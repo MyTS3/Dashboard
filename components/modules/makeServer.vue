27d71c1580b3ba55042015a6ff0155f6cfa6ffbe
mehdi <mehdibigi1383@gmail.com>
2024-11-16 18:40:55 +0330

    delete server fix

--- components/modules/makeServer.vue
+++ components/modules/makeServer.vue
@@ -10,10 +10,10 @@
         <button
           :disabled="disableInputs"
           :class="{ 'bg-main_red/30': disableInputs }"
-          class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
+          class="self-end text-center flex justify-center items-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
           @click="$emit('close')"
         >
-          X
+          <img class="w-3" src="/images/X-9.png" alt="" />
         </button>
         <header class="my-5">
           <h1 class="text-xl font-bold">ساخت سرور تیم اسپیک</h1>
--- components/modules/server/deleteServer.vue
+++ components/modules/server/deleteServer.vue
@@ -1,44 +1,47 @@
 <template>
-  <div>
-    <section
-      class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
-    >
-      <main
-        class="text-white min-w-96 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
+  <Teleport to="body">
+    <div>
+      <section
+        class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
       >
-        <button
-          :disabled="disable"
-          class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
-          @click="$emit('close')"
+        <main
+          class="text-white min-w-96 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
         >
-          X
-        </button>
-        <h1 class="text-xl my-4 font-bolder">حذف سرور</h1>
-        <p class="font-thin max-w-80 text-center mx-auto mb-4 mx-10">
-          این عمل قابل بازگردانی نیست و تمامی اطلاعات سرور را حذف میکند، اطمینان
-          <span class="font-bold">{{ selectedServer.name }}</span>
-          دارید؟
-        </p>
-        <div class="grid grid-cols-2 gap-3">
           <button
             :disabled="disable"
-            class="p-4 text-center rounded-xl border-2 border-blue-700/80 bg-blue-600/20 module-btn"
+            class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
             @click="$emit('close')"
           >
-            لغو
+            X
           </button>
-          <button
-            :disabled="disable"
-            class="p-4 flex justify-center text-center rounded-xl bg-main_red module-btn"
-            @click.prevent="deleteTheServer()"
-          >
-            <p v-if="!disable">حذف سرور</p>
-            <TheLoading v-else />
-          </button>
-        </div>
-      </main>
-    </section>
-  </div>
+          <h1 class="text-xl my-4 font-bolder">حذف سرور</h1>
+          <p class="font-thin max-w-80 text-center mx-auto mb-4 mx-10">
+            این عمل قابل بازگردانی نیست و تمامی اطلاعات سرور را حذف میکند،
+            اطمینان
+            <span class="font-bold">{{ selectedServer.name }}</span>
+            دارید؟
+          </p>
+          <div class="grid grid-cols-2 gap-3">
+            <button
+              :disabled="disable"
+              class="p-4 text-center rounded-xl border-2 border-blue-700/80 bg-blue-600/20 module-btn"
+              @click="$emit('close')"
+            >
+              لغو
+            </button>
+            <button
+              :disabled="disable"
+              class="p-4 flex justify-center text-center rounded-xl bg-main_red module-btn"
+              @click.prevent="deleteTheServer()"
+            >
+              <p v-if="!disable">حذف سرور</p>
+              <TheLoading v-else />
+            </button>
+          </div>
+        </main>
+      </section>
+    </div>
+  </Teleport>
 </template>
 <script setup>
 import TheLoading from '~/components/reusable/theLoading.vue';
