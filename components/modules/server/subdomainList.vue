590124cbb46ff8641c53d69edc506ac0c43a585f
mehdi <mehdibigi1383@gmail.com>
2024-11-18 12:38:13 +0330

    subdomain list skeleton added

--- components/modules/server/subdomainList.vue
+++ components/modules/server/subdomainList.vue
@@ -2,81 +2,156 @@
   <section
     class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
   >
-    <main
-      class="text-white min-w-[30rem] w-2/5 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
-    >
-      <button
-        :disabled="disable"
-        class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
-        @click="$emit('close')"
+    <template v-if="!subPend && !domPend">
+      <main
+        class="text-white min-w-[30rem] w-2/5 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
       >
-        X
-      </button>
-      <h1 class="my-4 text-3xl font-extrabold">لیست ساب دامنه ها</h1>
-      <div class="bg-mainbg_500 rounded-2xl">
-        <div
-          style="background-color: rgba(39, 43, 77, 1)"
-          class="table items items-center text-center rounded-2xl"
+        <button
+          :disabled="disable"
+          class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
+          @click="$emit('close')"
         >
-          <p class="">ساب</p>
-          <p class="">دامنه</p>
-          <p class="">اکشن</p>
-        </div>
-        <div class="overflow-scroll max-h-[16.5rem]">
+          X
+        </button>
+        <h1 class="my-4 text-3xl font-extrabold">لیست ساب دامنه ها</h1>
+        <div class="bg-mainbg_500 rounded-2xl">
           <div
-            v-for="(subdomain, i) in subDomainList"
-            :key="subdomain"
-            class="table items items-center text-center rounded-lg"
+            style="background-color: rgba(39, 43, 77, 1)"
+            class="table items items-center text-center rounded-2xl"
           >
-            <p class="">{{ subdomain.sub }}</p>
-            <p class="">{{ subdomain.domain.domain }}</p>
+            <p>ساب</p>
+            <p>دامنه</p>
+            <p>اکشن</p>
+          </div>
+          <div class="overflow-scroll max-h-[16.5rem]">
+            <div
+              v-for="(subdomain, i) in subDomainList"
+              :key="subdomain"
+              class="table items items-center text-center rounded-lg"
+            >
+              <p>{{ subdomain.sub }}</p>
+              <p>{{ subdomain.domain.domain }}</p>
+              <img
+                class="mx-auto cursor-pointer"
+                src="/images/trash.png"
+                alt=""
+                @click="deleteSubDomain(i)"
+              />
+            </div>
+          </div>
+          <div class="table items items-center text-center rounded-lg">
+            <input
+              v-model="subToAdd"
+              :disabled="disable"
+              class="w-2/3 mx-auto p-1.5 rounded-lg bg-transparent border text-right"
+              type="text"
+            />
+            <select
+              v-model="domainToAdd"
+              :disabled="disable"
+              class="w-2/3 mx-auto p-1.5 rounded-lg bg-transparent border text-right"
+            >
+              <option
+                v-for="domain in domainList"
+                :key="domain.uuid"
+                class="bg-mainbg_500"
+                :value="domain"
+              >
+                {{ domain.domain }}
+              </option>
+            </select>
             <img
               class="mx-auto cursor-pointer"
-              src="/images/trash.png"
+              src="/images/add-square.png"
               alt=""
-              @click="deleteSubDomain(i)"
+              @click="addToList()"
             />
           </div>
         </div>
-        <div class="table items items-center text-center rounded-lg">
-          <input
-            v-model="subToAdd"
-            :disabled="disable"
-            class="w-2/3 mx-auto p-1.5 rounded-lg bg-transparent border text-right"
-            type="text"
-          />
-          <select
-            v-model="domainToAdd"
-            :disabled="disable"
-            class="w-2/3 mx-auto p-1.5 rounded-lg bg-transparent border text-right"
-          >
-            <option
-              v-for="domain in domainList"
-              :key="domain.uuid"
-              class="bg-mainbg_500"
-              :value="domain"
-            >
-              {{ domain.domain }}
-            </option>
-          </select>
-          <img
-            class="mx-auto cursor-pointer"
-            src="/images/add-square.png"
-            alt=""
-            @click="addToList()"
-          />
-        </div>
-      </div>
-      <button
-        :class="disable ? 'opacity-45' : ''"
-        :disabled="disable"
-        class="w-full flex justify-center p-4 bg-main_blue rounded-xl my-2"
-        @click="submitSubdomains()"
+        <button
+          :class="disable ? 'opacity-45' : ''"
+          :disabled="disable"
+          class="w-full flex justify-center p-4 bg-main_blue rounded-xl my-2"
+          @click="submitSubdomains()"
+        >
+          <p v-if="!disable">اعمال تغییرات</p>
+          <TheLoading v-else />
+        </button>
+      </main>
+    </template>
+    <template v-else>
+      <main
+        class="text-white min-w-[30rem] w-2/5 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
       >
-        <p v-if="!disable">اعمال تغییرات</p>
-        <TheLoading v-else />
-      </button>
-    </main>
+        <button
+          :disabled="disable"
+          class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
+          @click="$emit('close')"
+        >
+          X
+        </button>
+        <header class="my-2">
+          <USkeleton
+            class="h-10 w-44 mx-auto"
+            :ui="{ background: 'dark:bg-gray-500' }"
+          />
+        </header>
+        <main class="grid gap-1 bg-mainbg_400 pb-4 rounded-lg">
+          <div
+            class="flex h-12 items-center justify-center bg-mainbg_300 rounded-lg"
+          >
+            <USkeleton
+              class="h-4 w-14 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+            <USkeleton
+              class="h-4 w-14 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+            <USkeleton
+              class="h-4 w-14 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+          </div>
+          <div class="flex h-12 items-center justify-center rounded-lg">
+            <USkeleton
+              class="h-4 w-14 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+            <USkeleton
+              class="h-4 w-14 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+            <USkeleton
+              class="h-4 w-14 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+          </div>
+          <div
+            class="grid grid-cols-3 h-12 items-center justify-center rounded-lg"
+          >
+            <USkeleton
+              class="h-10 w-2/3 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+            <USkeleton
+              class="h-10 w-2/3 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+            <USkeleton
+              class="h-4 w-14 mx-auto"
+              :ui="{ background: 'dark:bg-gray-500' }"
+            />
+          </div>
+        </main>
+        <footer>
+          <USkeleton
+            class="h-4 w-full p-6 mt-5 mx-auto"
+            :ui="{ background: 'dark:bg-gray-500' }"
+          />
+        </footer>
+      </main>
+    </template>
   </section>
 </template>
 <script setup>
@@ -91,46 +166,49 @@ const subToAdd = ref('');
 const disable = ref(false);
 const toast = useToast();
 
-async function getDomain() {
-  const { data: response, error } = await useFetch(
-    `${url.value}/api/v4/tdomains`,
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    },
-  );
-  domainList.value = await response.value;
-  if (error.value) {
-    toast.add({
-      title: 'خطایی رخ داد لطفا مجددا تلاش کنید',
-      timeout: 2000,
-      color: 'red',
-    });
-  }
+const {
+  data: domains,
+  error: domErr,
+  pending: subPend,
+} = await useFetch(`${url.value}/api/v4/tdomains`, {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
+});
+domainList.value = await domains.value;
+if (domErr.value) {
+  toast.add({
+    title: 'خطایی رخ داد لطفا مجددا تلاش کنید',
+    timeout: 2000,
+    color: 'red',
+  });
 }
 
 const subDomainList = ref();
 const props = defineProps(['selectedServer']);
-async function subDomains() {
-  const { data: respone, error } = await useFetch(
-    `${url.value}/api/v4/tservers/${props.selectedServer.uuid}/subdomains`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
+const {
+  data: subs,
+  error: subErr,
+  pending: domPend,
+} = await useFetch(
+  `${url.value}/api/v4/tservers/${props.selectedServer.uuid}/subdomains`,
+  {
+    method: 'GET',
+    lazy: true,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
     },
-  );
-  if (error.value) {
-    toast.add({
-      title: 'خطایی رخ داد لطفا مجددا تلاش کنید',
-      timeout: 2000,
-      color: 'red',
-    });
-  }
-  subDomainList.value = await respone.value;
+  },
+);
+if (subErr.value) {
+  toast.add({
+    title: 'خطایی رخ داد لطفا مجددا تلاش کنید',
+    timeout: 2000,
+    color: 'red',
+  });
 }
+subDomainList.value = await subs.value;
+
 function addToList() {
   subDomainList.value.push({
     sub: subToAdd.value,
@@ -156,6 +234,7 @@ async function submitSubdomains() {
     `${url.value}/api/v4/tservers/${props.selectedServer.uuid}/subdomains`,
     {
       method: 'PUT',
+      lazy: true,
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
@@ -174,8 +253,6 @@ async function submitSubdomains() {
   disable.value = false;
   emit('close');
 }
-await subDomains();
-await getDomain();
 </script>
 <style scoped>
 .table {
