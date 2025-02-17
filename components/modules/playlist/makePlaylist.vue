634d3b5c5b65fa52329d27849b9db266f46406cd
mehdi <mehdibigi1383@gmail.com>
2025-02-17 14:13:31 +0330

    make playlist component done

---
+++ components/modules/playlist/makePlaylist.vue
@@ -0,0 +1,75 @@
+<template>
+  <section
+    class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
+  >
+    <main
+      class="text-white min-w-[25rem] bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
+    >
+      <button
+        :disabled="disable"
+        class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
+        @click="$emit('close')"
+      >
+        <img class="w-3 mx-auto" src="/images/X-9.png" alt="" >
+      </button>
+      <h1 class="text-2xl my-4 font-extrabold">ساخت پلی لیست</h1>
+      <p class="text-sm text-white/80 mb-1">
+        نام پلی لیست را وارد کنید
+      </p>
+      <p class="font-bold max-w-80 text-center ml-auto">: نام</p>
+
+      <from class="w-full my-4">
+        <input v-model="playlistName" type="text"  class="p-3 w-full bg-transparent border-white flex justify-center rounded-xl border">
+        <USkeleton
+          v-if="pending"
+          class="h-11 w-full rounded-lg"
+          :ui="{ background: 'dark:bg-gray-500' }"
+        />
+      </from>
+      <div class="grid">
+        <button
+          :class="disable || pending ? 'disable' : ''"
+          :disabled="disable || pending"
+          class="p-4 text-center flex justify-center rounded-xl bg-main_blue module-btn"
+          @click="makePlaylist"
+          @click.prevent="changeConfigue()"
+        >
+          <p v-if="!disable">تایید</p>
+          <TheLoading v-else />
+        </button>
+      </div>
+    </main>
+  </section>
+</template>
+<script setup>
+import TheLoading from '@/components/reusable/theLoading.vue'
+const store = apiStore();
+const { url } = storeToRefs(store);
+const playlistName = ref()
+const disable = ref(false)
+const emit = defineEmits(['close'])
+async function makePlaylist(){
+  disable.value = true;
+  const { error } = await useFetch(
+    `${url.value}/api/v4/playlists`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify({
+        name: `${playlistName.value}`,
+      }),
+    },
+  );
+  if (error.value) {
+    toast.add({
+      title: 'خطایی رخ داد لطفا مجددا تلاش کنید',
+      timeout: 2000,
+      color: 'red',
+    });
+  }
+  disable.value = false;
+  emit('close');
+}
+</script>
--- pages/playlist/index.vue
+++ pages/playlist/index.vue
@@ -1,9 +1,80 @@
+
+<template>
+  <section
+    class="flex-1 w-full mx-auto flex flex-row items-stretch text-white text-center gap-layout min-h-0"
+  >
+    <main class="w-1/2 bg-mainbg_500 p-5 rounded-xl flex flex-col">
+      <header>
+        <p>پلی لیست ها</p>
+        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" >
+      </header>
+      <div class="bg-white/10 h-full mt-4 px-1 rounded-lg overflow-scroll">
+        <li
+          v-for="playlist in playlists"
+          :key="playlist.uuid"
+          :class="selectedPlaylistUuid == playlist.uuid ? 'btn-active' : 'hover:hover:bg-main_orange/20'"
+          class="list-none flex my-1 p-3  rounded-xl relative"
+          @click="selectedPlaylistUuid = playlist.uuid"
+        >
+          <div class="flex gap-layout">
+            <img class="w-5" src="/images/Folder.png" alt="" >
+            <h2 class="text-lg font-bold">{{ playlist.name }}</h2>
+          </div>
+          <div class="flex absolute bottom-1/2 translate-y-1/2 right-4">
+            <p class="font-sans text-xs my-auto font-bold">2GB</p>
+            <img src="/images/trash.png" >
+          </div>
+        </li>
+      </div>
+      <button
+      class="flex gap-btn w-full items-center justify-center btn rounded-xl py-3"
+        @click="makePlaylistTab=true"
+      >
+        ساخت پلی لیست
+        <img src="/images/addon.png" alt="" >
+      </button>
+    </main>
+    <main class="w-1/2 bg-mainbg_500 p-5 rounded-xl flex flex-col">
+      <header>
+        <p>آهنگ های پلی لیست</p>
+        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" >
+      </header>
+      <div class="bg-white/10 h-full mt-4 px-1 rounded-lg overflow-scroll">
+        <li
+          v-for="music in musics"
+          :key="music.uuid"
+          class="list-none flex my-1 p-3 hover:hover:bg-main_orange/20 rounded-xl relative"
+        >
+          <div class="flex gap-layout">
+            <img class="w-5" src="/images/music.png" alt="" >
+            <h2 class="text-lg font-bold">{{ music.name }}</h2>
+          </div>
+          <div class="flex absolute bottom-1/2 translate-y-1/2 right-4">
+            <p class="font-sans text-xs my-auto font-bold">20MB</p>
+            <img src="/images/trash.png" >
+          </div>
+        </li>
+      </div>
+      <button
+        class="flex gap-btn w-full items-center justify-center btn rounded-xl py-3"
+      >
+        افزودن موزیک
+        <img src="/images/addon.png" alt="" >
+      </button>
+    </main>
+    <MakePlaylist v-if="makePlaylistTab"  @close="makePlaylistTab=false,getPlaylist()" />
+  </section>
+</template>
+
 <script setup lang="ts">
+import MakePlaylist from '~/components/modules/playlist/makePlaylist.vue';
+
 const store = apiStore();
 const { url } = storeToRefs(store);
 const selectedPlaylistUuid = ref();
+const makePlaylistTab = ref(false);
 
-const { data: playlists } = await useFetch<{ uuid: string; name: string }[]>(
+const { data: playlists,execute:getPlaylist } = await useFetch<{ uuid: string; name: string }[]>(
   `${url.value}/api/v4/playlists`,
   {
     method: 'GET',
@@ -23,70 +94,3 @@ const { data: musics } = await useFetch<{ uuid: string; name: string }[]>(
   },
 );
 </script>
-
-<template>
-  <section
-    class="flex-1 w-full mx-auto flex flex-row items-stretch text-white text-center gap-layout min-h-0"
-  >
-    <main class="w-1/2 bg-mainbg_500 p-5 rounded-xl flex flex-col">
-      <header>
-        <p>پلی لیست ها</p>
-        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" />
-      </header>
-      <div class="bg-white/10 h-full mt-4 px-1 rounded-lg overflow-scroll">
-        <li
-          v-for="playlist in playlists"
-          :key="playlist.uuid"
-          @click="selectedPlaylistUuid = playlist.uuid"
-          :class="selectedPlaylistUuid == playlist.uuid ? 'btn-active' : ''"
-          class="list-none flex my-1 p-3 hover:hover:bg-main_orange/20 rounded-xl relative"
-        >
-          <div class="flex gap-layout">
-            <img class="w-5" src="/images/Folder.png" alt="" />
-            <h2 class="text-lg font-bold">{{ playlist.name }}</h2>
-          </div>
-          <div class="flex absolute bottom-1/2 translate-y-1/2 right-4">
-            <p class="font-sans text-xs my-auto font-bold">2GB</p>
-            <img src="/images/trash.png" />
-          </div>
-        </li>
-      </div>
-      <button
-        class="flex gap-btn w-full items-center justify-center btn rounded-xl py-3"
-      >
-        ساخت پلی لیست
-        <img src="/images/addon.png" alt="" />
-      </button>
-    </main>
-    <main class="w-1/2 bg-mainbg_500 p-5 rounded-xl flex flex-col">
-      <header>
-        <p>آهنگ های پلی لیست</p>
-        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" />
-      </header>
-      <div class="bg-white/10 h-full mt-4 px-1 rounded-lg overflow-scroll">
-        <li
-          v-for="music in musics"
-          :key="music.uuid"
-          class="list-none flex my-1 p-3 hover:hover:bg-main_orange/20 rounded-xl relative"
-        >
-          <div class="flex gap-layout">
-            <img class="w-5" src="/images/music.png" alt="" />
-            <h2 class="text-lg font-bold">{{ music.name }}</h2>
-          </div>
-          <div class="flex absolute bottom-1/2 translate-y-1/2 right-4">
-            <p class="font-sans text-xs my-auto font-bold">20MB</p>
-            <img src="/images/trash.png" />
-          </div>
-        </li>
-      </div>
-      <button
-        class="flex gap-btn w-full items-center justify-center btn rounded-xl py-3"
-      >
-        افزودن موزیک
-        <img src="/images/addon.png" alt="" />
-      </button>
-    </main>
-  </section>
-</template>
-
-<script setup></script>
