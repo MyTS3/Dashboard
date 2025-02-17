e506977f247187163fb8226617d958ec18a91fef
mehdi <mehdibigi1383@gmail.com>
2025-02-17 22:00:13 +0330

    format

--- pages/playlist/index.vue
+++ pages/playlist/index.vue
@@ -5,7 +5,7 @@
     <main class="w-1/2 bg-mainbg_500 p-5 rounded-xl flex flex-col">
       <header>
         <p>پلی لیست ها</p>
-        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" />
+        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" >
       </header>
       <div class="bg-white/10 h-full mt-4 px-1 rounded-lg overflow-scroll">
         <li
@@ -20,7 +20,7 @@
           @click="selectedPlaylistUuid = playlist.uuid"
         >
           <div class="flex gap-layout">
-            <img class="w-5" src="/images/Folder.png" alt="" />
+            <img class="w-5" src="/images/Folder.png" alt="" >
             <h2 class="text-lg font-bold">{{ playlist.name }}</h2>
           </div>
           <div class="flex absolute bottom-1/2 translate-y-1/2 right-4">
@@ -29,7 +29,7 @@
               class="cursor-pointer"
               src="/images/trash.png"
               @click="(removingPlaylist = playlist), (DeletePlaylistTab = true)"
-            />
+            >
           </div>
         </li>
       </div>
@@ -38,13 +38,13 @@
         @click="makePlaylistTab = true"
       >
         ساخت پلی لیست
-        <img src="/images/addon.png" alt="" />
+        <img src="/images/addon.png" alt="" >
       </button>
     </main>
     <main class="w-1/2 bg-mainbg_500 p-5 rounded-xl flex flex-col">
       <header>
         <p>آهنگ های پلی لیست</p>
-        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" />
+        <img class="w-full mt-3" src="/images/seprator-line.png" alt="" >
       </header>
       <div class="bg-white/10 h-full mt-4 px-1 rounded-lg overflow-scroll">
         <li
@@ -53,21 +53,22 @@
           class="list-none flex my-1 p-3 hover:hover:bg-main_orange/20 rounded-xl relative"
         >
           <div class="flex gap-layout">
-            <img class="w-5" src="/images/music.png" alt="" />
+            <img class="w-5" src="/images/music.png" alt="" >
             <h2 class="text-lg font-bold">{{ music.name }}</h2>
           </div>
           <div class="flex absolute bottom-1/2 translate-y-1/2 right-4">
             <p class="font-sans text-xs my-auto font-bold">20MB</p>
-            <img src="/images/trash.png" />
+            <img src="/images/trash.png" >
           </div>
         </li>
       </div>
       <button
+        :disabled="selectedPlaylistUuid==undefined"
         class="flex gap-btn w-full items-center justify-center btn rounded-xl py-3"
         @click="addMusicTab = true"
       >
         افزودن موزیک
-        <img src="/images/addon.png" alt="" />
+        <img src="/images/addon.png" alt="" >
       </button>
     </main>
     <MakePlaylist
@@ -94,11 +95,12 @@ import AddMusic from '~/components/modules/playlist/addMusic.vue';
 
 const store = apiStore();
 const { url } = storeToRefs(store);
-const selectedPlaylistUuid = ref();
+const selectedPlaylistUuid = ref(undefined);
 const DeletePlaylistTab = ref(false);
 const makePlaylistTab = ref(false);
 const addMusicTab = ref(false);
 const removingPlaylist = ref();
+
 const { data: playlists, execute: getPlaylist } = await useFetch<
   { uuid: string; name: string }[]
 >(`${url.value}/api/v4/playlists`, {
