2d880bb9911eecaff781b2acae5378ca6e339e98
mehdi <mehdibigi1383@gmail.com>
2025-02-22 11:53:24 +0330

    minor fix

--- components/modules/server/yatqaExample.vue
+++ components/modules/server/yatqaExample.vue
@@ -1,88 +1,90 @@
 <template>
-  <section
-    class="h-full absolute z-50 min-w-full backdrop-blur-md flex flex-col justify-center top-0 left-0 items-center"
-  >
-    <main
-      class="relative text-white min-w-[50rem] flex flex-col text-center p-4 relative rounded-xl font-medium"
+  <Teleport to="body">
+    <section
+      class="h-full absolute z-50 min-w-full backdrop-blur-md flex flex-col justify-center top-0 left-0 items-center"
     >
-      <button
-        class="text-center w-7 h-7 flex justify-center items-center bg-main_red absolute top-7 right-7 rounded-full text-mainbg_600 font-medium text-lg"
-        @click="$emit('close')"
+      <main
+        class="relative text-white min-w-[50rem] flex flex-col text-center p-4 relative rounded-xl font-medium"
       >
-        <img class="w-3" src="/images/X-9.png" alt="" />
-      </button>
+        <button
+          class="text-center w-7 h-7 flex justify-center items-center bg-main_red absolute top-7 right-7 rounded-full text-mainbg_600 font-medium text-lg"
+          @click="$emit('close')"
+        >
+          <img class="w-3" src="/images/X-9.png" alt="" />
+        </button>
 
-      <div
-        class="absolute parent items-center border-black border-[1px] cursor-pointer left-[122px] top-[146px] bg-white text-black w-[10.4rem] h-5 flex justify-around text-xs"
-        @click="copyToClipboard(selectedServer.name)"
-      >
-        <p class="text-black">{{ selectedServer.name }}</p>
-        <button>
-          <img class="w-4 child" src="/images/copy.svg" alt="" />
-        </button>
-      </div>
-      <div
-        class="absolute parent items-center border-black border-[1px] cursor-pointer left-[300px] top-[146px] bg-white text-black w-[4.6rem] h-5 flex justify-around text-xs"
-        @click="copyToClipboard(selectedServer.queryPort)"
-      >
-        <p>{{ selectedServer.queryPort }}</p>
-        <button>
-          <img class="w-4 child" src="/images/copy.svg" alt="" />
-        </button>
-      </div>
-      <div
-        class="absolute parent items-center border-black border-[1px] cursor-pointer left-[385px] top-[146px] bg-white text-black w-[4.6rem] h-5 flex justify-around text-xs"
-        @click="copyToClipboard(selectedServer.queryPort)"
-      >
-        <p>{{ selectedServer.queryPort }}</p>
-        <button>
-          <img class="w-4 child" src="/images/copy.svg" alt="" />
-        </button>
-      </div>
-      <div
-        class="absolute parent items-center border-black border-[1px] cursor-pointer left-[470px] top-[146px] bg-white text-black w-[7.2rem] h-5 flex justify-around text-xs"
-        @click="copyToClipboard('serveradmin')"
-      >
-        <p>serveradmin</p>
-        <button>
-          <img class="w-4 child" src="/images/copy.svg" alt="" />
-        </button>
-      </div>
-      <div
-        class="absolute items-center border-black border-[1px] cursor-pointer left-[590px] top-[146px] bg-white text-black w-[9rem] h-5 flex justify-around text-xs"
-      >
-        <div class="flex gap-1 parent">
-          <p v-if="newPass">{{ newPass }}</p>
-          <p
-            v-else
-            :style="{ '-webkit-text-security': showYatqaPass }"
-            @click="copyToClipboard(selectedServer.queryPassword)"
-          >
-            {{ selectedServer.queryPassword }}
-          </p>
-          <button
-            class="w"
-            @click="copyToClipboard(selectedServer.queryPassword)"
-          >
+        <div
+          class="absolute parent items-center border-black border-[1px] cursor-pointer left-[122px] top-[146px] bg-white text-black w-[10.4rem] h-5 flex justify-around text-xs"
+          @click="copyToClipboard(selectedServer.name)"
+        >
+          <p class="text-black">{{ selectedServer.name }}</p>
+          <button>
             <img class="w-4 child" src="/images/copy.svg" alt="" />
           </button>
         </div>
+        <div
+          class="absolute parent items-center border-black border-[1px] cursor-pointer left-[300px] top-[146px] bg-white text-black w-[4.6rem] h-5 flex justify-around text-xs"
+          @click="copyToClipboard(selectedServer.queryPort)"
+        >
+          <p>{{ selectedServer.queryPort }}</p>
+          <button>
+            <img class="w-4 child" src="/images/copy.svg" alt="" />
+          </button>
+        </div>
+        <div
+          class="absolute parent items-center border-black border-[1px] cursor-pointer left-[385px] top-[146px] bg-white text-black w-[4.6rem] h-5 flex justify-around text-xs"
+          @click="copyToClipboard(selectedServer.queryPort)"
+        >
+          <p>{{ selectedServer.queryPort }}</p>
+          <button>
+            <img class="w-4 child" src="/images/copy.svg" alt="" />
+          </button>
+        </div>
+        <div
+          class="absolute parent items-center border-black border-[1px] cursor-pointer left-[470px] top-[146px] bg-white text-black w-[7.2rem] h-5 flex justify-around text-xs"
+          @click="copyToClipboard('serveradmin')"
+        >
+          <p>serveradmin</p>
+          <button>
+            <img class="w-4 child" src="/images/copy.svg" alt="" />
+          </button>
+        </div>
+        <div
+          class="absolute items-center border-black border-[1px] cursor-pointer left-[590px] top-[146px] bg-white text-black w-[9rem] h-5 flex justify-around text-xs"
+        >
+          <div class="flex gap-1 parent">
+            <p v-if="newPass">{{ newPass }}</p>
+            <p
+              v-else
+              :style="{ '-webkit-text-security': showYatqaPass }"
+              @click="copyToClipboard(selectedServer.queryPassword)"
+            >
+              {{ selectedServer.queryPassword }}
+            </p>
+            <button
+              class="w"
+              @click="copyToClipboard(selectedServer.queryPassword)"
+            >
+              <img class="w-4 child" src="/images/copy.svg" alt="" />
+            </button>
+          </div>
 
-        <button @click.prevent="changeYatqaPass()">
-          <img
-            class="w-4 hover:opacity-50"
-            src="/images/change_nickname.svg"
-            alt=""
-          />
-        </button>
-      </div>
-      <img
-        class="w-full h-full rounded-3xl pic"
-        src="/images/yatqa-example3.png"
-        alt=""
-      />
-    </main>
-  </section>
+          <button @click.prevent="changeYatqaPass()">
+            <img
+              class="w-4 hover:opacity-50"
+              src="/images/change_nickname.svg"
+              alt=""
+            />
+          </button>
+        </div>
+        <img
+          class="w-full h-full rounded-3xl pic"
+          src="/images/yatqa-example3.png"
+          alt=""
+        />
+      </main>
+    </section>
+  </Teleport>
 </template>
 <script setup>
 const newPass = ref(undefined);
