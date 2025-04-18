7a0df4c28f65b0364120a9bc0f43696c3b6598e3
mehdi <mehdibigi1383@gmail.com>
2025-04-18 19:46:33 +0330

    table change

--- components/reusable/table.vue
+++ components/reusable/table.vue
@@ -1,9 +1,32 @@
 <template>
   <section dir="rtl" class="w-full grid h-full mx-auto text-white">
     <main
+      ref="table"
       class="w-full overflow-y-scroll flex flex-col bg-mainbg_500 rounded-2xl rounded-t-none"
     >
       <slot />
     </main>
   </section>
 </template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useInfiniteScroll } from '@vueuse/core';
+const table = ref(null);
+const emits = defineEmits(['nextPage']);
+const props = defineProps(['backups']);
+onMounted(() => {
+  if (!table.value) return;
+
+  useInfiniteScroll(
+    table,
+    async () => {
+      emits('nextPage');
+      console.log('end');
+    },
+    {
+      distance: 10,
+      canLoadMore: () => props.backups.length == 10,
+    },
+  );
+});
+</script>
