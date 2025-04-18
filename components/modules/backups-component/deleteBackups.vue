b198ec06f5641482b93ee1f8cb388f8aec4f4e02
mehdi <mehdibigi1383@gmail.com>
2025-04-18 20:50:37 +0330

    fix

--- components/modules/backups-component/deleteBackups.vue
+++ components/modules/backups-component/deleteBackups.vue
@@ -1,39 +1,41 @@
 <template>
-  <section
-    class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
-  >
-    <main
-      class="text-white min-w-96 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
+  <Teleport to="body">
+    <section
+      class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
     >
-      <button
-        class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
-        @click="$emit('close')"
+      <main
+        class="text-white min-w-96 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
       >
-        <img class="w-3 mx-auto" src="/images/X-9.png" alt="" />
-      </button>
-      <h1 class="my-4">حذف بکاپ</h1>
-      <p class="mb-4">شما در حال پاک کردن بکاپ هستید ایا اطمینان دارید؟</p>
-      <div class="grid grid-cols-2 gap-3">
         <button
-          :class="disable ? 'disable' : ''"
-          :disabled="disable"
-          class="p-4 text-center rounded-xl border-2 border-blue-700/80 bg-blue-600/20 module-btn"
+          class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
           @click="$emit('close')"
         >
-          لغو
+          <img class="w-3 mx-auto" src="/images/X-9.png" alt="" />
         </button>
-        <button
-          :class="disable ? 'disable' : ''"
-          :disabled="disable"
-          class="p-4 flex justify-center items-center text-center rounded-xl bg-main_red module-btn"
-          @click.prevent="removeBackup()"
-        >
-          <p v-if="!disable">تایید</p>
-          <ReusableTheLoading v-else />
-        </button>
-      </div>
-    </main>
-  </section>
+        <h1 class="my-4">حذف بکاپ</h1>
+        <p class="mb-4">شما در حال پاک کردن بکاپ هستید ایا اطمینان دارید؟</p>
+        <div class="grid grid-cols-2 gap-3">
+          <button
+            :class="disable ? 'disable' : ''"
+            :disabled="disable"
+            class="p-4 text-center rounded-xl border-2 border-blue-700/80 bg-blue-600/20 module-btn"
+            @click="$emit('close')"
+          >
+            لغو
+          </button>
+          <button
+            :class="disable ? 'disable' : ''"
+            :disabled="disable"
+            class="p-4 flex justify-center items-center text-center rounded-xl bg-main_red module-btn"
+            @click.prevent="removeBackup()"
+          >
+            <p v-if="!disable">تایید</p>
+            <ReusableTheLoading v-else />
+          </button>
+        </div>
+      </main>
+    </section>
+  </Teleport>
 </template>
 <script setup>
 const store = apiStore();
--- components/modules/backups-component/restoreBackup.vue
+++ components/modules/backups-component/restoreBackup.vue
@@ -1,48 +1,54 @@
 <template>
-  <section
-    class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
-  >
-    <main
-      class="text-white max-w-[30rem] bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
+  <Teleport to="body">
+    <section
+      class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
     >
-      <button
-        :disabled="disable"
-        class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
-        @click="$emit('close')"
+      <main
+        class="text-white max-w-[30rem] bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
       >
-        <img class="w-3 mx-auto" src="/images/X-9.png" alt="" />
-      </button>
-      <h1 class="text-2xl my-4 font-extrabold">ریستور بک آپ</h1>
-      <p class="text-sm text-main_red mb-2">
-        این عمل باعث میشود تمامی اطلاعات جدید سرور شما پاک شود و سرور شما به
-        زمانی که بکاپ را گرفته اید برگرداند، آیا اطمینان دارید؟
-      </p>
-      <p class="font-bold max-w-80 text-center ml-auto">
-        سرور خود را انتخاب کنید
-      </p>
-      <from class="w-full my-4">
-        <select
-          v-model="tserverUUid"
-          class="w-full bg-transparent text-right appearance-none border rounded-xl p-3"
-          name="locations"
-        >
-          <option v-for="server in servers" :key="server" :value="server.uuid">
-            {{ server.name }}
-          </option>
-        </select>
-      </from>
-      <div class="grid">
         <button
           :disabled="disable"
-          class="p-4 text-center rounded-xl flex justify-center bg-main_red module-btn"
-          @click="deployBackup()"
+          class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
+          @click="$emit('close')"
         >
-          <p v-if="!disable">تایید</p>
-          <TheLoading v-else />
+          <img class="w-3 mx-auto" src="/images/X-9.png" alt="" />
         </button>
-      </div>
-    </main>
-  </section>
+        <h1 class="text-2xl my-4 font-extrabold">ریستور بک آپ</h1>
+        <p class="text-sm text-main_red mb-2">
+          این عمل باعث میشود تمامی اطلاعات جدید سرور شما پاک شود و سرور شما به
+          زمانی که بکاپ را گرفته اید برگرداند، آیا اطمینان دارید؟
+        </p>
+        <p class="font-bold max-w-80 text-center ml-auto">
+          سرور خود را انتخاب کنید
+        </p>
+        <from class="w-full my-4">
+          <select
+            v-model="tserverUUid"
+            class="w-full bg-transparent text-right appearance-none border rounded-xl p-3"
+            name="locations"
+          >
+            <option
+              v-for="server in servers"
+              :key="server"
+              :value="server.uuid"
+            >
+              {{ server.name }}
+            </option>
+          </select>
+        </from>
+        <div class="grid">
+          <button
+            :disabled="disable"
+            class="p-4 text-center rounded-xl flex justify-center bg-main_red module-btn"
+            @click="deployBackup()"
+          >
+            <p v-if="!disable">تایید</p>
+            <TheLoading v-else />
+          </button>
+        </div>
+      </main>
+    </section>
+  </Teleport>
 </template>
 <script setup>
 const store = apiStore();
