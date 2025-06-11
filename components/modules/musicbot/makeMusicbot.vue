8d11c9b5084533599250fa786cf52b3056405cfe
Mehdi <mehdibigi1383@gmail.com>
2025-06-11 21:06:14 +0330

    teleport added to make music bot

--- components/modules/musicbot/makeMusicbot.vue
+++ components/modules/musicbot/makeMusicbot.vue
@@ -1,109 +1,113 @@
 <template>
-  <section
-    class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
-  >
-    <main
-      class="text-white min-w-96 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
+  <teleport to="body">
+    <section
+      class="h-full absolute z-50 w-full backdrop-blur-md bg-mainbg_500/20 flex justify-center top-0 left-0 items-center"
     >
-      <button
-        :disabled="disableInputs"
-        class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
-        @click="$emit('close')"
+      <main
+        class="text-white min-w-96 bg-mainbg_600 flex flex-col text-center border border-white border-b-0 p-4 relative rounded-xl font-medium"
       >
-        <img class="w-3 mx-auto" src="/images/X-9.png" alt="" />
-      </button>
-      <header>
-        <h1 class="text-lg font-bold">ساخت موزیک بات</h1>
-        <p class="text-x mt-3 mb-6 mx-20 font-light">
-          برای ساخت پلی لیست موزیک بات خودرا اتتخاب کنید
-        </p>
-      </header>
-      <div>
-        <p class="font-bold w-full text-right">پلی لیست</p>
+        <button
+          :disabled="disableInputs"
+          class="self-end text-center w-7 h-7 bg-main_red absolute top-3 right-3 rounded-full text-mainbg_600 font-medium text-lg"
+          @click="$emit('close')"
+        >
+          <img class="w-3 mx-auto" src="/images/X-9.png" alt="" />
+        </button>
+        <header>
+          <h1 class="text-lg font-bold">ساخت موزیک بات</h1>
+          <p class="text-x mt-3 mb-6 mx-20 font-light">
+            برای ساخت پلی لیست موزیک بات خودرا اتتخاب کنید
+          </p>
+        </header>
+        <div>
+          <p class="font-bold w-full text-right">پلی لیست</p>
 
-        <!-- <select
-          v-model="selectedPlaylist"
-          class="text-white bg-transparent my-4 border w-full text-right min-h-16 rounded-2xl"
-          name="music"
+          <!-- <select
+    v-model="selectedPlaylist"
+    class="text-white bg-transparent my-4 border w-full text-right min-h-16 rounded-2xl"
+    name="music"
+    >
+    <option
+    v-for="playlist in playlists"
+    :key="playlist.uuid"
+    :value="playlist"
+    >
+    {{ playlist.name }}
+  </option>
+</select> -->
+          <USelectMenu
+            v-model="selectedPlaylist"
+            class="w-full my-4"
+            size="xl"
+            color="indigo"
+            :options="playlists"
+          />
+          <!-- ///////////////////price///////////// -->
+          <div id="price" class="w-full">
+            <div class="flex justify-between flex-row-reverse text-white/40">
+              <h1>:قیمت ساعتی</h1>
+              <div class="flex flex-row-reverse gap-1 text-white/40">
+                <span>{{
+                  Math.abs(
+                    Math.floor(
+                      (botPrice.price / botPrice.secondsForPrice) * 3600,
+                    ),
+                  ) ?? 0
+                }}</span>
+                <p>تومان</p>
+              </div>
+            </div>
+            <div
+              class="flex justify-between flex-row-reverse mt-3 text-white/40"
+            >
+              <h1>:قیمت روزانه</h1>
+              <div class="flex flex-row-reverse gap-1 text-white/40">
+                <span>{{
+                  Math.abs(
+                    Math.floor(
+                      (botPrice.price / botPrice.secondsForPrice) * 86400,
+                    ),
+                  ) ?? 0
+                }}</span>
+                <p>تومان</p>
+              </div>
+            </div>
+            <div class="flex justify-between flex-row-reverse mt-3">
+              <h1>:قیمت ماهانه</h1>
+              <div class="flex flex-row-reverse gap-1">
+                <span>{{
+                  Math.abs(
+                    Math.floor(
+                      (botPrice.price / botPrice.secondsForPrice) * 2629800,
+                    ),
+                  ) ?? 0
+                }}</span>
+                <p>تومان</p>
+              </div>
+            </div>
+          </div>
+          <!-- ////////////////////////////////// -->
+        </div>
+        <button
+          :disabled="!selectedPlaylist"
+          :class="{
+            'cursor-not-allowed opacity-30': disableInputs || !selectedPlaylist,
+          }"
+          class="flex w-full items-center justify-center make-server font-medium gap-2 mt-4"
+          @click="makeMusicBot()"
         >
-          <option
-            v-for="playlist in playlists"
-            :key="playlist.uuid"
-            :value="playlist"
+          <div
+            v-if="!disableInputs"
+            class="w-full flex justify-center gap-2 items-center"
           >
-            {{ playlist.name }}
-          </option>
-        </select> -->
-        <USelectMenu
-          v-model="selectedPlaylist"
-          class="w-full my-4"
-          size="xl"
-          color="indigo"
-          :options="playlists"
-        />
-        <!-- ///////////////////price///////////// -->
-        <div id="price" class="w-full">
-          <div class="flex justify-between flex-row-reverse text-white/40">
-            <h1>:قیمت ساعتی</h1>
-            <div class="flex flex-row-reverse gap-1 text-white/40">
-              <span>{{
-                Math.abs(
-                  Math.floor(
-                    (botPrice.price / botPrice.secondsForPrice) * 3600,
-                  ),
-                ) ?? 0
-              }}</span>
-              <p>تومان</p>
-            </div>
+            <span><img src="/images/plus.png" alt="" /></span>
+            <p>ساخت</p>
           </div>
-          <div class="flex justify-between flex-row-reverse mt-3 text-white/40">
-            <h1>:قیمت روزانه</h1>
-            <div class="flex flex-row-reverse gap-1 text-white/40">
-              <span>{{
-                Math.abs(
-                  Math.floor(
-                    (botPrice.price / botPrice.secondsForPrice) * 86400,
-                  ),
-                ) ?? 0
-              }}</span>
-              <p>تومان</p>
-            </div>
-          </div>
-          <div class="flex justify-between flex-row-reverse mt-3">
-            <h1>:قیمت ماهانه</h1>
-            <div class="flex flex-row-reverse gap-1">
-              <span>{{
-                Math.abs(
-                  Math.floor(
-                    (botPrice.price / botPrice.secondsForPrice) * 2629800,
-                  ),
-                ) ?? 0
-              }}</span>
-              <p>تومان</p>
-            </div>
-          </div>
-        </div>
-        <!-- ////////////////////////////////// -->
-      </div>
-      <button
-        :disabled="!selectedPlaylist"
-        :class="{
-          'cursor-not-allowed opacity-30': disableInputs || !selectedPlaylist,
-        }"
-        class="flex w-full items-center justify-center make-server font-medium gap-2 mt-4"
-        @click="makeMusicBot()"
-      >
-        <div
-          v-if="!disableInputs"
-          class="w-full flex justify-center gap-2 items-center"
-        >
-          <span><img src="/images/plus.png" alt="" /></span>
-          <p>ساخت</p>
-        </div>
-        <TheLoading v-if="disableInputs" />
-      </button>
-    </main>
-  </section>
+          <TheLoading v-if="disableInputs" />
+        </button>
+      </main>
+    </section>
+  </teleport>
 </template>
 <script setup>
 import TheLoading from '~/components/reusable/theLoading.vue';
