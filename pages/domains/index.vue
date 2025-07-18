c8daeafac271b0d92b00e3c971d5c5efa7a6113b
Mehdi <mehdibigi1383@gmail.com>
2025-07-18 14:10:21 +0330

    domain skeleton fix

--- pages/domains/index.vue
+++ pages/domains/index.vue
@@ -1,109 +1,106 @@
 <template>
   <div class="h-full flex flex-col min-h-0 relative">
-    <template v-if="status == 'success'">
-      <div dir="rtl" class="table bg-mainbg_300 rounded-t-2xl">
-        <p>دامنه</p>
-        <p>وضعیت</p>
-        <p>تاریخ ثبت</p>
-        <p>عمل</p>
-      </div>
-      <div class="overflow-y-auto h-full bg-mainbg_400">
-        <Table class="flex-1 min-h-0 overflow-y-auto">
-          <div class="h-full">
-            <template v-if="status == 'pending'">
-              <div v-for="_ in 5" :key="_" class="table items">
-                <USkeleton
-                  class="h-5 w-40"
-                  :ui="{ background: 'dark:bg-gray-500' }"
-                />
-                <USkeleton
-                  class="h-5 w-20"
-                  :ui="{ background: 'dark:bg-gray-500' }"
-                />
-                <USkeleton
-                  class="h-5 w-20"
-                  :ui="{ background: 'dark:bg-gray-500' }"
-                />
-                <USkeleton
-                  class="h-5 w-10"
-                  :ui="{ background: 'dark:bg-gray-500' }"
-                />
+    <div dir="rtl" class="table bg-mainbg_300 rounded-t-2xl">
+      <p>دامنه</p>
+      <p>وضعیت</p>
+      <p>تاریخ ثبت</p>
+      <p>عمل</p>
+    </div>
+    <div class="overflow-y-auto h-full bg-mainbg_400">
+      <Table class="flex-1 min-h-0 overflow-y-auto">
+        <div class="h-full">
+          <template v-if="status == 'pending'">
+            <div v-for="_ in 5" :key="_" class="table items">
+              <USkeleton
+                class="h-5 w-40"
+                :ui="{ background: 'dark:bg-gray-500' }"
+              />
+              <USkeleton
+                class="h-5 w-20"
+                :ui="{ background: 'dark:bg-gray-500' }"
+              />
+              <USkeleton
+                class="h-5 w-20"
+                :ui="{ background: 'dark:bg-gray-500' }"
+              />
+              <USkeleton
+                class="h-5 w-10"
+                :ui="{ background: 'dark:bg-gray-500' }"
+              />
+            </div>
+          </template>
+          <template v-if="status == 'success'">
+            <div
+              v-if="domainList.length < 1"
+              class="flex flex-col w-full h-full justify-center gap-4 items-center"
+            >
+              <img
+                class="min-w-[15rem] w-[10vw]"
+                src="/images/new/no-domain.png"
+                alt=""
+              />
+              <div class="grid text-center text-[1vw]">
+                <p class="font-bold text-white">هیچ دامنه ای وجود ندارد</p>
+                <p>با کلیک برروی دکمه زیر اولین دامنه خودرا بسازید</p>
               </div>
-            </template>
-            <template v-if="status == 'success'">
+            </div>
+            <div v-for="domain in domainList" :key="domain" class="table items">
+              <p class="font-semibold">{{ domain.domain }}</p>
               <div
-                v-if="domainList.length < 1"
-                class="flex flex-col w-full h-full justify-center gap-4 items-center"
+                v-if="domain.active"
+                class="flex w-20 justify-center items-center rounded-3xl h-8 bg-main_green/15"
               >
-                <img
-                  class="min-w-[15rem] w-[10vw]"
-                  src="/images/new/no-domain.png"
-                  alt=""
-                />
-                <div class="grid text-center text-[1vw]">
-                  <p class="font-bold text-white">هیچ دامنه ای وجود ندارد</p>
-                  <p>با کلیک برروی دکمه زیر اولین دامنه خودرا بسازید</p>
-                </div>
+                <img class="w-5" src="/images/check.png" alt="" />
+                <p class="font-bold text-sm text-main_green">فعال</p>
               </div>
               <div
-                v-for="domain in domainList"
-                :key="domain"
-                class="table items"
+                v-else
+                class="flex w-20 justify-center items-center rounded-3xl h-8 bg-main_orange/15"
               >
-                <p class="font-semibold">{{ domain.domain }}</p>
-                <div
-                  v-if="domain.active"
-                  class="flex w-20 justify-center items-center rounded-3xl h-8 bg-main_green/15"
-                >
-                  <img class="w-5" src="/images/check.png" alt="" />
-                  <p class="font-bold text-sm text-main_green">فعال</p>
-                </div>
-                <div
-                  v-else
-                  class="flex w-20 justify-center items-center rounded-3xl h-8 bg-main_orange/15"
-                >
-                  <img class="w-5" src="/images/waiting.png" alt="" />
-                  <p class="font-bold text-sm text-main_orange">در انتظار</p>
-                </div>
-                <p>{{ timeAgo.format(new Date(domain.createdAt)) }}</p>
-                <img
-                  v-if="!domain.public"
-                  class="cursor-pointer"
-                  src="/images/trash.png"
-                  alt=""
-                  @click="(deleteDomainTab = true), (selectedDomain = domain)"
-                />
+                <img class="w-5" src="/images/waiting.png" alt="" />
+                <p class="font-bold text-sm text-main_orange">در انتظار</p>
               </div>
-            </template>
-          </div>
-        </Table>
-      </div>
-      <UTooltip
-        v-if="
-          limits &&
-          domainList &&
-          domainList.filter((d) => !d.public).length >= limits.value.maxDomains
-        "
-        :text="'شما به حداکثر تعداد دامنه های خود رسیده اید'"
-      >
-        <button
-          :disabled="true"
-          class="flex w-full gap-btn items-center justify-center btn-disable rounded-xl py-3"
-          @click="addDomainTab = true"
-        >
-          افزودن دامنه
-          <img src="/images/addon.png" alt="" />
-        </button>
-      </UTooltip>
+              <p>{{ timeAgo.format(new Date(domain.createdAt)) }}</p>
+              <img
+                v-if="!domain.public"
+                class="cursor-pointer"
+                src="/images/trash.png"
+                alt=""
+                @click="(deleteDomainTab = true), (selectedDomain = domain)"
+              />
+            </div>
+          </template>
+        </div>
+      </Table>
+    </div>
+    <UTooltip
+      v-if="
+        limits &&
+        domainList &&
+        domainList.filter((d) => !d.public).length >= limits.value.maxDomains
+      "
+      :text="'شما به حداکثر تعداد دامنه های خود رسیده اید'"
+    >
       <button
-        v-else
-        class="flex w-full gap-btn items-center justify-center btn rounded-xl py-3"
+        :disabled="true"
+        class="flex w-full gap-btn items-center justify-center btn-disable rounded-xl py-3"
         @click="addDomainTab = true"
       >
         افزودن دامنه
         <img src="/images/addon.png" alt="" />
       </button>
-      <!-- <div
+    </UTooltip>
+    <button
+      :disabled="status != 'success'"
+      :class="status != 'success' ? 'opacity-50' : ''"
+      v-else
+      class="flex w-full gap-btn items-center justify-center btn rounded-xl py-3"
+      @click="addDomainTab = true"
+    >
+      افزودن دامنه
+      <img src="/images/addon.png" alt="" />
+    </button>
+    <!-- <div
         v-if="domainList.length < 1"
         class="w-full absolute top-1/4 flex flex-col justify-center gap-4 items-center"
       >
@@ -113,7 +110,7 @@
           <p>با کلیک برروی دکمه زیر اولین دامین خودرا بسازید</p>
         </div>
       </div> -->
-    </template>
+
     <AddDomain
       v-if="addDomainTab"
       @close="getDomain(), (addDomainTab = false)"
@@ -141,7 +138,7 @@ const selectedDomain = ref();
 const {
   data: domainList,
   status: status,
-  execute: getDomain,
+  refresh: getDomain,
 } = await useFetch(`${url.value}/api/v4/tdomains`, {
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
