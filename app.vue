1fd5b461ded252527f896a2c4197f644c5b5e3fc
Daniel <daniel@sohe.ir>
2025-10-03 18:13:40 +0330

    chore: goftino disabled temporary

--- app.vue
+++ app.vue
@@ -9,7 +9,7 @@
 <script setup lang="ts">
 useHead({
   script: [
-    '!function(){var i="3P2eY6",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();',
+    // '!function(){var i="3P2eY6",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();',
   ],
 });
 </script>
